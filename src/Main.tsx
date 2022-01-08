4da3dbb373fd6ff7035cebd6cb7f6af4f5d3f783
Rudel <rudik005@gmail.com>
2022-01-08 17:26:25 +0400

    init

---
+++ src/Main.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Main : FC = () => {
+    return (
+        <></>
+    )
+};
+
+export default Main;
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import { AppBar } from '@mui/material'
+import React from 'react'
+
+export const Sidebar = () => {
+    return (
+        <AppBar>
+            
+        </AppBar>
+    )
+}
